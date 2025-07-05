83ab35f29ec0f3fdc23beeb04295dd88aabd0d0f
Adbelss <adbel1311@gmail.com>
2025-07-04 20:49:48 -0600

    Día 11.2: Corrección de formulario, estilos Bootstrap y funcionalidad completa

--- consultas/forms.py
+++ consultas/forms.py
@@ -5,3 +5,9 @@ class ConsultaForm(forms.ModelForm):
     class Meta:
         model = Consulta
         fields = ['paciente_nombre', 'motivo', 'diagnostico', 'tratamiento']
+        widgets = {
+            'paciente_nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'motivo': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'diagnostico': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'tratamiento': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
